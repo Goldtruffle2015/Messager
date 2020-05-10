e4c66da957b1fc3556dd2a7fbe3a5f6b139382e9
John Yu <goldtruffle2015@hotmail.com>
2020-05-09 21:33:43 -0600

    Cleaned up the code

--- Messager/src/Network/Server.java
+++ Messager/src/Network/Server.java
@@ -1,18 +1,13 @@
 package Network;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
 	// -- Attributes -- // 
-	private String[] args;
 	private ServerSocket serverSocket;
 	private Socket socket;
 	private ClientHandler[] clients = new ClientHandler[2];
